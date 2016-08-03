be4a22c129bd4cb1dc89606a32533da180eedf76
KIMHYUNSEUNG <nopy79@gmail.com>
2016-08-04 00:49:35 +0900

    첫번째 커밋입니다.

--- Zimzomair/ViewController.swift
+++ Zimzomair/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://zimzom.co.kr");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
